d2cb6584ff35b4c9d112be08477d79d15cbdcbe5
Carlos Montilla <carlosdmontilla@gmail.com>
2016-02-27 12:39:01 +0100

    stats formated in right data

--- stats/stats.py
+++ stats/stats.py
@@ -6,6 +6,22 @@ Programmed by: Carlos Montilla
 import os
 
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def convertToFloat(s):
+    if is_number(s):
+        return float(s)
+    else:
+        return s
+
+def convertListToFloat(lst):
+    return [convertToFloat(elem) for elem in lst]
+
 def getData(pathdir):
     f = open(pathdir + "score.org")
     name = f.readline()
@@ -19,7 +35,8 @@ def getData(pathdir):
     for line in f:
         data.append(line.rstrip().replace(" ","").split("|")[1:-1])
 
-    return data
+
+    return [convertListToFloat(ls) for ls in data]
 def main():
     testDir = "../easy/findHighestScore/"
     print getData(testDir)
