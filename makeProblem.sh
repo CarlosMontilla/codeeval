6029c19479c804e2f2bdad98e574e55af3fbafa1
Carlos Montilla <carlosdmontilla@gmail.com>
2016-02-27 13:18:20 +0100

    Level and problem code input added

--- makeProblem.sh
+++ makeProblem.sh
@@ -4,6 +4,17 @@
 # and the org file for the scores
 # Programmed by: Carlos Montilla
 
+echo "Enter the level of the problem:"
+echo "1: easy"
+echo "2: Medium"
+echo "3: Hard"
+read problemLevel
+
+if [ $problemLevel -lt 1 ] || [ $problemLevel -gt 3 ]; then
+    echo "Error! Invalid problem level"
+    exit 1
+fi
+
 echo "Enter the name of the problem: "
 read problemName
 
@@ -14,3 +25,11 @@ fi
 
 echo "Enter the code of the problem [Optional]: "
 read problemCode
+
+
+if [ "$problemCode" = "" ]; then
+    problemUrl=unknown
+else
+    problemUrl="https://www.codeeval.com/open_challenges/"$problemCode
+fi
+echo $problemUrl
