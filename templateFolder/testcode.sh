9cf720929b3e0bb7012f476a74990ec7c36cb2a9
Carlos Montilla <carlosdmontilla@gmail.com>
2016-03-01 17:37:32 +0100

    file to test code

---
+++ templateFolder/testcode.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Code for testing the solution of a codeeval problem
+# Programmed by: Carlos Montilla
+
+filename=main
+inputfile=./in
+outputfile=./out
+
+
+case $1 in
+    "cpp") g++ -o cpp.out main.cpp && implementation=compiler;;
+    "c") gcc -o c.out main.c && implementation=compiler ;;
+    "f90") gfortran -o f90.out main.f90 && implementation=compiler ;;
+    "py") implementation=interpreter ;;
+    "py3") implementation=interpreter ;;
+    "rb") implementation=interpreter ;;
+    "sh") implementation=interpreter ;;
+    *) echo "Unknown extension!"; exit 1;;
+esac
+
+if [ $implementation == compiler ]; then
+    ./$1.out $inputfile > $1.results
+else
+    ./$filename.$1 > $1.results
+fi
+
+nTest=`wc -l < $outputfile`
+diff=`diff -U 0 $outputfile $1.results | grep -v ^@ | wc -l`
+if [ diff -eq 0 ]; then
+    echo "All tests passed"
+else
+    failed=
+    echo -n "$diff / $nTest tests failed"
+fi
