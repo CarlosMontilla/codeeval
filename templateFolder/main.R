076da814fcc80bbf13f8e2fbc20d8af008f7ecd9
Carlos Montilla <carlosdmontilla@gmail.com>
2016-03-01 17:47:16 +0100

    R code with shebang

--- templateFolder/main.R
+++ templateFolder/main.R
@@ -1,8 +1,7 @@
+#!/usr/bin/env Rscript
+
 args <- commandArgs(trailingOnly=TRUE)
 test.cases <- strsplit(readLines(args[[1]], warn=FALSE), '\n')
 for (test in test.cases) {
-   if (length(test) > 0) {
-       # ignore test if it is an empty line
-       # 'test' represents the test case, do something with it
-   }
+    print("Hello World")
 }
