ae8f15a20f856597889eb2eb4bdee1887a89edcf
Carlos Montilla <carlosdmontilla@gmail.com>
2016-03-11 23:34:29 +0100

    details in c++

--- easy/details/main.cpp
+++ easy/details/main.cpp
@@ -1,12 +1,49 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
 using namespace std;
+
+int getDistance(string);
 
 int main(int argc, char *argv[]) {
     ifstream stream(argv[1]);
     string line;
     while (getline(stream, line)) {
-        cout << "Hello World!" << endl;
+        char row_del = ',';
+        int start = 0;
+        size_t pos;
+        int max_dist = 11;
+        do {
+            pos = line.find(row_del, start);
+            string row = line.substr(start, pos - start);
+            int dis = getDistance(row);
+            max_dist = (dis < max_dist) ? dis : max_dist;
+            start = pos + 1;
+            if (max_dist == 0)
+                break;
+        }while (pos != string::npos);
+        cout << max_dist << endl;
     }
     return 0;
+}
+
+int getDistance(string row){
+    int distance = 0;
+    char left_detail = 'X';
+    char space = '.';
+    char right_detail = 'Y';
+    for (int i = 0; i < row.size(); i++){
+        if (row[i] == left_detail){
+            distance = 0;
+        }
+        else if (row[i] == space){
+            distance++;
+        }
+        else{
+            return distance;
+        }
+    }
+
+    return distance;
 }
