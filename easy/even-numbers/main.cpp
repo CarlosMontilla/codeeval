e8d9140aa4baa3f619a2cfb78e6499e779b4e16a
Carlos Montilla <carlosdmontilla@gmail.com>
2016-03-11 23:42:41 +0100

    even numbers in c++

--- easy/even-numbers/main.cpp
+++ easy/even-numbers/main.cpp
@@ -6,31 +6,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
+
 
 using namespace std;
 
 bool isEven(int);
-int toInt(string);
+
 
 int main(int argc, char* argv[]){
     ifstream stream(argv[1]);
     string line;
     while(getline(stream, line)){
-        int n = toInt(line);
-        cout << (int) isEven(n) << endl;
+        cout << (int) isEven(stoi(line)) << endl;
     }
 }
 
 
 bool isEven(int n){
     return (n % 2) == 0;
-}
-
-int toInt(string s){
-    int value;
-    stringstream convert(s);
-    if(!(convert >> value))
-        value = 0;
-    return value;
 }
--- easy/even-numbers/main.rb
+++ easy/even-numbers/main.rb
@@ -1,7 +1,7 @@
+#!/usr/bin/env ruby
 # Checks if a given number is even
 def isEven?(n)
   if n % 2 == 0 then 1 else 0 end
 end
 
 File.open(ARGV[0]).read.split("\n").each {|i| puts isEven?(i.to_i)}
-
