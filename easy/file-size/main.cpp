3d36a7b2b1259261b516680f09b9a8ef59825fe5
Carlos Montilla <carlosdmontilla@gmail.com>
2016-03-12 21:21:01 +0100

    File size in c++

---
+++ easy/file-size/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+    FILE *file;
+    long size;
+    file = fopen(argv[1], "rb");
+    fseek(file, 0, SEEK_END);
+    size = ftell(file);
+    fclose(file);
+    cout << size << endl;
+
+    return 0;
+}
--- easy/file-size/main.rb
+++ easy/file-size/main.rb
@@ -1,2 +1,2 @@
+#!/usr/bin/env ruby
 puts File.size(ARGV[0])
-
