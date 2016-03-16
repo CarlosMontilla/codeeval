c81202bf96a763784366beb170ebaba4eda2e15a
Carlos Montilla <carlosdmontilla@gmail.com>
2016-03-16 12:55:24 +0100

    hex to decimal problem in c++

--- easy/hex-to-decimal/main.cpp
+++ easy/hex-to-decimal/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+
 using namespace std;
 
 int main(int argc, char *argv[]) {
     ifstream stream(argv[1]);
     string line;
     while (getline(stream, line)) {
-        cout << "Hello World!" << endl;
+        stringstream line_stream;
+        int dec;
+        line_stream << hex << line;
+        line_stream >> dec;
+        cout << dec << endl;
     }
     return 0;
 }
--- easy/hex-to-decimal/main.rb
+++ easy/hex-to-decimal/main.rb
@@ -1,3 +1,3 @@
+#!/usr/bin/env ruby
 # Converts an hex number into decimal one
 File.open(ARGV[0]).read.split("\n").each {|s| puts s.to_i(16)}
-
